4e31b2e3e6fa70390b9efec7b0804725800065c3
byronleng <byrongleng@tencent.com>
2020-08-18 19:17:30 +0800

    initialize

---
+++ src/main/java/com/byron/base/BaseUtils.java
@@ -0,0 +1,14 @@
+package com.byron.base;
+
+import com.byron.db.DbUtils;
+import com.byron.net.NetUtils;
+
+public class BaseUtils {
+    public static String getNetHello(){
+        return NetUtils.getHello();
+    }
+
+    public static String getDbHello(){
+        return DbUtils.getHello();
+    }
+}
